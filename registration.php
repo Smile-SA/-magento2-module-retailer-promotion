0ea1b35718188789620bbde0021dd4d5e3f32e05
Fanny DECLERCK <fanny.declerck@smile.fr>
2019-08-13 15:22:23 +0200

    Init module

---
+++ registration.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @category  Smile
+ * @package   Smile\RetailerPromotion
+ * @author    Fanny DECLERCK <fadec@smile.fr>
+ * @copyright 2019 Smile
+ * @license   Open Software License ("OSL") v. 3.0
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Smile_RetailerPromotion',
+    __DIR__
+);
