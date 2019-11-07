66a1d1d781e8655869cc647aa5e1d197731b1909
Ihor KVASNYTSKYI <ihor.kvasnytskyi@smile-ukraine.com>
2019-11-07 16:22:15 +0200

    ihkva_Change#821992 replace promotion slider from StoreLocator module to current module

---
+++ view/frontend/requirejs-config.js
@@ -0,0 +1,23 @@
+/**
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this module to newer
+ * versions in the future.
+ *
+ *
+ * @category  Smile
+ * @package   Smile\Map
+ * @author    Romain Ruaud <romain.ruaud@smile.fr>
+ * @author    Guillaume Vrac <guillaume.vrac@smile.fr>
+ * @copyright 2016 Smile
+ * @license   Open Software License ("OSL") v. 3.0
+ */
+
+var config = {
+    map: {
+        '*': {
+            'slick'                  : 'Smile_RetailerPromotion/js/lib/slick.min',
+            'promotionSlider'        : 'Smile_RetailerPromotion/js/promotionSlider'
+        }
+    }
+};
